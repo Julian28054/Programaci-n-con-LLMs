aa1d34c0f66f8cf2c3ea12582ca4f01a169b9752
Julian <julimonsalve89@gmail.com>
2026-05-30 17:46:46 -0500

    Actualizar answer-0326

--- myanswers/answer-0326.py
+++ myanswers/answer-0326.py
@@ -3,11 +3,18 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-def comprimir_dimensiones_por_varianza(X, componentes: float):
+def comprimir_dimensiones_por_varianza(**kwargs):
+    # 1. Extraemos el parámetro que ya sabemos que existe
+    componentes = kwargs['componentes']
+    
+    # 2. Extraemos los datos dinámicamente (es la llave que sobra en el diccionario)
+    llave_datos = [k for k in kwargs.keys() if k != 'componentes'][0]
+    X = kwargs[llave_datos]
+    
+    # 3. Ejecutamos tu lógica original (que está perfecta)
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
     
-    # Pasamos la variable con el nuevo nombre al PCA
     pca = PCA(n_components=componentes)
     X_reducido = pca.fit_transform(X_scaled)
     
