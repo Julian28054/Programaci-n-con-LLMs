0f6ca6f93c8356c5edf248110fd023b8307afbfc
Julian28054 <julimonsalve89@gmail.com>
2026-05-30 15:59:02 -0500

    Update answer-0326.py

--- myanswers/answer-0326.py
+++ myanswers/answer-0326.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
-def comprimir_dimensiones_por_varianza(X=None, umbral_varianza=0.95, componentes=None):
-
-    datos = componentes if componentes is not None else X
-
-    pca = PCA()
-    pca.fit(datos)
-
-    return pca.explained_variance_ratio_.tolist()
+def comprimir_dimensiones_por_varianza(umbral_varianza=0.95, X=None, componentes=None):
+    """Documentación"""
+    if X is None:
+        X = pd.DataFrame({
+            "var1": [10, 12, 11, 13, 12, 11, 300, 10],
+            "var2": [20, 22, 21, 23, 22, 21, 400, 20],
+            "var3": [30, 32, 31, 33, 32, 31, 500, 30]
+        })
+    
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    pca = PCA(n_components=umbral_varianza)
+    X_reducido = pca.fit_transform(X_scaled)
+    
+    return {"componentes": X_reducido.tolist()}, pca.explained_variance_ratio_.tolist()
