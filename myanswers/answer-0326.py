4b6ad2439ad4cd759dd8465d552eeb0ca087dc37
Julian <julimonsalve89@gmail.com>
2026-05-30 17:40:41 -0500

    Actualizar answer-0326

--- myanswers/answer-0326.py
+++ myanswers/answer-0326.py
@@ -1,12 +1,14 @@
-def comprimir_dimensiones_por_varianza(componentes, umbral_varianza: float):
-    import numpy as np
-    from sklearn.decomposition import PCA
-    from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
+def comprimir_dimensiones_por_varianza(X, componentes: float):
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(componentes)
+    X_scaled = scaler.fit_transform(X)
     
-    pca = PCA(n_components=umbral_varianza)
+    # Pasamos la variable con el nuevo nombre al PCA
+    pca = PCA(n_components=componentes)
     X_reducido = pca.fit_transform(X_scaled)
     
-    return X_reducido, pca.explained_variance_ratio_.tolist()
+    return X_reducido
